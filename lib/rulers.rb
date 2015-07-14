ac68b59520ff0500db7c369dcc2c4dfbc11da08c
squareleaf <tiffany.green@wellbe.me>
2015-07-13 21:11:55 -0500

    camel case to snake case

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,12 +1,34 @@
 require "rulers/version"
-require "rulers/array"
+require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
-      "echo debug > debug.txt";
-      [200, {"Content-Type" => "text/html"},
-        ["Hello from Ruby on Rulers!"]]
+      if env["PATH_INFO"] == "/favicon.ico"
+        return [404, 
+          {"Content-Type" => "text/html"}, []]
+      end
+      if env["PATH-INFO"] == "/"
+        return "/quotes/a_quote"
+      end
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      begin
+        text = controller.send(act)
+      rescue
+      end
+      [200, {'Content-Type' => 'text/html'},
+        [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
